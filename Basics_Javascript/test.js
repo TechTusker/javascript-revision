8f313d5ef699783c1593fd9c5de34d79d76dfe5b
PANIC PALS! <147988686+TechTusker@users.noreply.github.com>
2025-01-01 19:12:52 +0000

    container config done

--- Basics_Javascript/test.js
+++ Basics_Javascript/test.js
@@ -1,7 +1,13 @@
 console.log("Shreya is the Best!")
+
 //Variables and constants
 //registration of the user on portal providing unique id and getting their information
 const accountId = 1223354
 let accountEmail = "shreyaaa@yahoo.com"
 var pswd = "12345"
 accountCity = "Jaipur"
+//investigating the properties of the keywords
+accountEmail = "shre@gmail.com"
+pswd = "2345"
+accountCity = "Bengaluru"
+console.table([accountId, accountEmail, pswd, accountCity])
