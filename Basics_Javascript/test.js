1165fcf43bfca56489d0b22d1735eba55fbe7b38
PANIC PALS! <147988686+TechTusker@users.noreply.github.com>
2025-01-01 17:34:37 +0000

    container config done

--- Basics_Javascript/test.js
+++ Basics_Javascript/test.js
@@ -1,1 +1,7 @@
 console.log("Shreya is the Best!")
+//Variables and constants
+//registration of the user on portal providing unique id and getting their information
+const accountId = 1223354
+let accountEmail = "shreyaaa@yahoo.com"
+var pswd = "12345"
+accountCity = "Jaipur"
